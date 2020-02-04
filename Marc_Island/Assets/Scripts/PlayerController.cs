6ee071fe8d8e3e0a2fab2434709ae9fc3702bc69
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-04 19:48:39 +0100

    Player, Enemy & Power Up

--- Marc_Island/Assets/Scripts/PlayerController.cs
+++ Marc_Island/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool hasPowerup;
+    public float powerupStrength = 15;
     private Rigidbody playerRB;
     public float speed;
     GameObject focalPoint;
@@ -23,5 +25,32 @@ public class PlayerController : MonoBehaviour
         // now we need the player to move in the direction of the camera so..."
         playerRB.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime);
 
+    }
+
+   /* IEnumerator PowerupCountdownRoutine()
+    {
+
+
+    }
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Power Up"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRB = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 pushAwayEnemy = (other.gameObject.transform.position - transform.position);
+            Debug.Log("Player collied with" + other.gameObject + " with powerup set to " + hasPowerup);
+            enemyRB.AddForce(pushAwayEnemy * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
--- Marc_Island/Assets/Scripts/SpawnManager.cs
+++ Marc_Island/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    float spawnRange = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        // something to make spawn
+        Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        float spawnRandomX = Random.Range(-spawnRange, spawnRange);
+        float spawnRandomZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnRandomPosition = new Vector3(spawnRandomX, 0, spawnRandomZ);
+        Debug.Log("Random position" + spawnRandomPosition);
+        return spawnRandomPosition;
     }
 }
