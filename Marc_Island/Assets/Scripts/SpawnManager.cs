9f38e7dfe7b36b06dbc4218df5d89f8ec56842d8
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-25 19:44:41 +0100

    SpawnManager

--- Marc_Island/Assets/Scripts/SpawnManager.cs
+++ Marc_Island/Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     float spawnRange = 10;
     public float minMassRange = 0.5f;
     public float maxMassRange = 2.5f;
+    public GameObject player;
 
     int waves = 1;
     public GameObject powerUpPrefab;
@@ -20,12 +21,30 @@ public class SpawnManager : MonoBehaviour
         SpawnEnemyWave(waves);
         // something to make spawn
         Instantiate(powerUpPrefab, GenerateRandomPosition(), powerUpPrefab.transform.rotation);
+
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int enemiesCount = FindObjectsOfType<EnemyMovement>().Lenght
+        // find all the gameObjects in the scene with the component EnemyScript
+        if (enemiesCount == 0 && !isGameOver) // there are no more enemies
+        {
+            waves++;
+            SpawnEnemyWave(waves);
+            Instantiate(powerUpPrefab, GenerateRandomPosition(), powerUpPrefab.transform.rotation);
+        }
+        if (player.transform.position.y <= -10) // the player falls from the island
+        {
+            isGameOver = true;
+            gameOverText.enabled = true;
+            if (Input.anyKeyDown) // the player has dissapeared
+            {
+                RestartGame();
+            }
+        }
     }
 
     Vector3 GenerateRandomPosition()
@@ -41,7 +60,15 @@ public class SpawnManager : MonoBehaviour
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
+            GameObject enemy = Instantiate(enemyPrefab, GenerateRandomPosition(), enemyPrefab.transform.rotation);
+            Rigidbody enemyRB = enemy.GetComponent<Rigidbody>();
+            EnemyMovement enemyScript = enemy.GetComponent<EnemyMovement>();
+
+            enemyRB.mass = Random.Range(minMassRange, maxMassRange);
+            float currentMass = enemyRB.mass;
+            enemy.transform.localScale = new Vector3(currentMass, currentMass, currentMass);
+            // current scale is changed according to the randomized mass of the enemy spawned
+            enemyScript.speed = enemyScript.speed + currentMass;
         }
     }
 }
