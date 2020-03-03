10bebbdd3410144ad7e0c8a104ee3d8e6e1eec3a
Edukative <56083723+Edukative@users.noreply.github.com>
2020-03-03 19:49:48 +0100

    SpawnManager

--- Marc_Island/Assets/Scripts/SpawnManager.cs
+++ Marc_Island/Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class SpawnManager : MonoBehaviour
     public float minMassRange = 0.5f;
     public float maxMassRange = 2.5f;
     public GameObject player;
+    bool isGameOver = false;
+    public Text gameOverText;
+    GameObject canvas;
+    Vector3 initialPosition;
 
     int waves = 1;
     public GameObject powerUpPrefab;
@@ -23,12 +28,16 @@ public class SpawnManager : MonoBehaviour
         Instantiate(powerUpPrefab, GenerateRandomPosition(), powerUpPrefab.transform.rotation);
 
         player = GameObject.Find("Player");
+        initialPosition = player.transform.position;
+        canvas = GameObject.Find("Canvas");
+        gameOverText = canvas.transform.GetChild(0).GetComponent<Text>();
+        gameOverText.enabled = false; // set unactive the text
     }
 
     // Update is called once per frame
     void Update()
     {
-        int enemiesCount = FindObjectsOfType<EnemyMovement>().Lenght
+        int enemiesCount = FindObjectsOfType<EnemyMovement>().Length;
         // find all the gameObjects in the scene with the component EnemyScript
         if (enemiesCount == 0 && !isGameOver) // there are no more enemies
         {
@@ -54,6 +63,29 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnRandomPosition = new Vector3(spawnRandomX, 0, spawnRandomZ);
         Debug.Log("Random position" + spawnRandomPosition);
         return spawnRandomPosition;
+    }
+
+    void RestartGame ()
+    {
+        player.transform.position = initialPosition;
+        waves = 1;
+        SpawnEnemyWave(waves);
+        isGameOver = false;
+        gameOverText.enabled = false;
+
+        int powerupCount = GameObject.FindGameObjectsWithTag("Powerup").Length; //get the number if powerups
+        if (powerupCount == 0) // there is no powerup in the scene
+        {
+            Instantiate(powerUpPrefab, GenerateRandomPosition(), powerUpPrefab.transform.rotation); // spawns 1
+        }
+        else if (powerupCount > 1) // more than 1 powerup in the island
+        {
+            for (int i = 0; i < powerupCount; i++)  // in all the powerups except 1...
+            {
+                GameObject PowerupToDestroy = GameObject.FindGameObjectsWithTag("Poweup"); // find 1 gameObject with this tag...
+                Destroy(PowerupToDestroy); // and destroy it
+            }
+        }
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
