c67ff43f0654277c3c4273924b2d4fa233306695
Edukative <56083723+Edukative@users.noreply.github.com>
2020-03-10 19:46:17 +0100

    Pause Menu

---
+++ Marc_Island/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenu = "Main Menu";
+
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+    
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        //FindObjectsOfType<GameManager>().Reset();
+    }
+
+    public void QuitToMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
